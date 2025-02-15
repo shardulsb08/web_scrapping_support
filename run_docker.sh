03ee9a45ffe4e4d1243b73b00e14f88ae3188b3b
Shardul Bankar <sbankar@cloudlinux.com>
2025-02-15 14:41:58 +0530

    Pass URL link to the script in container non-interactive mode

--- run_docker.sh
+++ run_docker.sh
@@ -3,5 +3,12 @@
 # Build the Docker image and tag it as 'scrapping_tool'
 docker build -t scrapping_tool .
 
-# Run the container and remove it after exit
-docker run --rm scrapping_tool
+# Check if a URL argument was provided; if not, prompt the user
+if [ "$#" -eq 0 ]; then
+    read -p "Enter the URL to analyze: " url
+else
+    url="$1"
+fi
+
+# Run the container, passing the URL as an argument
+docker run --rm scrapping_tool "$url"
